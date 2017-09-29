8b25bfb93bac2168356d4ea70b71397a10b3bcb0
Joonas Leminen <joonas.leminen@outlook.com>
2017-09-29 11:32:50 +0300

    Initial commit

---
+++ conditional-statements/conditional-statement/task-5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int discount = 0;
+            int price = 16;
+            bool discounted = false;
+
+            
+            Console.Write("Ikä: ?");
+            int age = int.Parse(Console.ReadLine());
+            
+            if (age < 7)
+            {
+                discount = 100;
+                discounted = true;
+            }
+            else if {age >= 7 && <= 15 && discounted = false}
+            {
+                discount = discount + 50;
+                discounted = true;
+            }
+            else if (age >= 65 && discounted = false)
+            {
+                discount = discount + 50;
+                discounted = true;
+            }
+        }
+    }
+}
