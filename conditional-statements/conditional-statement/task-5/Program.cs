21794bdd12351f6f1182a6e1976288533ab2f8c8
Joonas Leminen <joonas.leminen@outlook.com>
2017-10-06 15:32:58 +0300

    Initial commit

--- conditional-statements/conditional-statement/task-5/Program.cs
+++ conditional-statements/conditional-statement/task-5/Program.cs
@@ -10,7 +10,6 @@ namespace task_5
     {
         static void Main(string[] args)
         {
-
             string firstName = "Uusi";
             string lastName = "Asiakas";
             Console.WriteLine("Anna nimesi: ");
@@ -18,8 +17,8 @@ namespace task_5
             lastName = Console.ReadLine();
             Console.WriteLine("Nimi: " + firstName + " " + lastName);
 
-            int discount = 0;
-            int price = 16;
+            float discount = 0;
+            float price = 16;
             bool discounted = false;
             
             Console.Write("Anna ikäsi: ");
@@ -30,18 +29,62 @@ namespace task_5
                 discount = 100;
                 discounted = true;
             }
-            else if (age >= 7 && <= 15 && discounted = false);
+            else if (age >= 7 && age <= 15 && discounted == false)
             {
                 discount = discount + 50;
                 discounted = true;
             }
-            
-            else if (age >= 65 && discounted = false);
+            else if (age >= 65 && discounted == false)
             {
                 discount = discount + 50;
                 discounted = true;
             }
-
+            else if (age > 15 && age < 65)
+            {
+                Console.WriteLine("Oletko varusmies? Y/N");
+                string conscriptResponse = Console.ReadLine().ToUpper();
+                {
+                    if (conscriptResponse == "Y")
+                    {
+                        discount = discount + 50;
+                        discounted = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Oletko Mtk:n jäsen? Y/N");
+                        string mtkResponse = Console.ReadLine().ToUpper();
+                        {
+                            if (mtkResponse == "Y")
+                            {
+                                discount = discount + 15;
+                                discounted = true;
+                            }
+                            else if (mtkResponse == "N")
+                            {
+                                discount = discount + 0;
+                                discounted = false;
+                            }
+                        }
+                        Console.WriteLine("Oletko opiskelija? Y/N");
+                        string studentResponse = Console.ReadLine().ToUpper();
+                        {
+                            if (studentResponse == "Y")
+                            {
+                                discount = discount + 45;
+                                discounted = true;
+                            }
+                            else if (studentResponse == "N")
+                            {
+                                discount = discount + 0;
+                                discounted = false;
+                            }
+                        }
+                    }
+                }
+            }
+            float finalPrice = price - ((price * discount) / 100);
+            Console.WriteLine($"Lippusi hinta on {finalPrice}e");
+            Console.ReadKey();
         }
     }
 }
