4f4ce687075cb407f621181c872a28de87534e8c
Joonas Leminen <joonas.leminen@outlook.com>
2017-10-12 17:28:53 +0300

    Initial commit

---
+++ loop-statements/loop-statements/task-2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee N ensimmäistä lukua yhteen.");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+            int i = 1;
+            int f = 0;
+            do
+            {
+                i = i + 1;
+                f = i * 1;
+            } while (i < number);
+            Console.Write($"Syötit: {number}, Summa: {f}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ loop-statements/loop-statements/task-3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee ensimmäisen ja parittoman ja parillisen luvun summan N:stä.");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+            int i = 1;
+            int f = 0;
+            do
+            {
+
+            } while ();
+        }
+    }
+}
