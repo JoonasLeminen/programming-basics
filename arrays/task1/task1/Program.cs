1e094e5f8dba02d9c26e74447a63eadd2649b62a
Joonas Leminen <joonas.leminen@outlook.com>
2017-11-09 09:44:20 +0200

    Arrays2 kesken

---
+++ arrays/task1/task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taulukko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tee ohjelma, joka arpoo 100 lukua [0...50] väliltä ja tallettaa ne taulukkoon iT. Arvotut luvut sekä luvuista saatu keskiarvo, joka ilmoitetaan käyttäjälle.");
+            Random rnd = new Random();
+            int[] iT;
+            iT = new int[100];
+            double sum = 0;
+
+            for (int i = 0; i < iT.Length; i++)
+            {
+                iT[i] = rnd.Next(0, 50);
+                Console.WriteLine($"{iT[i]}");
+            }
+
+            for (int i = 0; i < iT.Length; i++)
+            {
+                sum += iT[i];
+            }
+            double ka = sum / 100;
+            Console.WriteLine($"Keskiarvo: {ka}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ arrays/task1/task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma arpoo 7 eri lukua 1-40 ja tulostaa arvotut luvut pienimmästä suurimpaan.");
+            Random rnd = new Random();
+            int[] iT;
+            iT = new int[7];
+
+            for (int i = 0; i < 7; i++)
+            {
+                iT[i] = rnd.Next(1,39);
+                iT[i] = iT[1];
+            }
+            for (int i = 0; i < iT.Length; i++)
+            {
+                Console.WriteLine($"{iT[1]}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
