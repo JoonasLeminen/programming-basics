460061582364ae8d25f103a39504b1c74e33b25b
Joonas Leminen <joonas.leminen@outlook.com>
2017-11-10 11:00:59 +0200

    Taulukot3 kesken

--- arrays/task1/task2/Program.cs
+++ arrays/task1/task2/Program.cs
@@ -13,18 +13,31 @@ namespace task2
             Console.WriteLine("Ohjelma arpoo 7 eri lukua 1-40 ja tulostaa arvotut luvut pienimmästä suurimpaan.");
             Random rnd = new Random();
             int[] iT;
-            iT = new int[7];
+            iT = new int[9];
 
-            for (int i = 0; i < 7; i++)
+            for (int i = 0; i < 8; i++)
             {
-                iT[i] = rnd.Next(1,39);
-                iT[i] = iT[1];
-            }
-            for (int i = 0; i < iT.Length; i++)
-            {
-                Console.WriteLine($"{iT[1]}");
+                iT[i] = rnd.Next(1, 40);
+
+                for (int j = 0; j < i; j++)
+                {
+                    if (iT[i] == iT[j])
+                    {
+                        i--;
+                    }
+                }
             }
 
+            Array.Sort(iT, null, 0,7);
+            Console.WriteLine("Loton oikea rivi on:");
+            for (int i = 0; i < iT.Length - 2; i++)
+            {
+                Console.Write($"{iT[i]} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"Lisänumero: {iT[7]}");
+            Console.WriteLine($"Tuplausnumero: {rnd.Next(1, 40)}");
             Console.ReadKey();
         }
     }
---
+++ arrays/task1/task3.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Taulukkoharjoitukset 3.1");
+            Random rnd = new Random();
+            int[] iT;
+            iT = new int[10];
+            for (int i = 0; i < iT.Length; i++)
+            {
+
+            }
+        }
+        static string numbers()
+        {
+
+        }
+
+    }
+}
